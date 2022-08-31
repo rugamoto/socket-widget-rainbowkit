4f69fd97e65313eb3518411be300b6c29f48275a
rugamoto <apetoshi.rugamoto@gmail.com>
2022-08-31 11:17:02 +0100

    rainbowkit + socket widget working

---
+++ components/Widget.tsx
@@ -0,0 +1,1 @@
+import { Bridge } from "@socket.tech/widget";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import dynamic from "next/dynamic";
+import { useSigner } from "wagmi";
+import { providers } from "ethers";
+const DynamicBridge = dynamic(
+  // @ts-ignore
+  () => import("@socket.tech/widget").then((mod) => mod.Bridge),
+  {
+    ssr: false,
+  }
+);
 
 const Home: NextPage = () => {
+  const { data: signer } = useSigner();
+  const web3Provider =
+    signer?.provider &&
+    new providers.Web3Provider((signer?.provider as any).provider);
   return (
     <div className={styles.container}>
       <Head>
@@ -18,58 +32,13 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <ConnectButton />
 
-        <h1 className={styles.title}>
-          Welcome to <a href="">RainbowKit</a> + <a href="">wagmi</a> +{' '}
-          <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://rainbowkit.com" className={styles.card}>
-            <h2>RainbowKit Documentation &rarr;</h2>
-            <p>Learn how to customize your wallet connection flow.</p>
-          </a>
-
-          <a href="https://wagmi.sh" className={styles.card}>
-            <h2>wagmi Documentation &rarr;</h2>
-            <p>Learn how to interact with Ethereum.</p>
-          </a>
-
-          <a
-            href="https://github.com/rainbow-me/rainbowkit/tree/main/examples"
-            className={styles.card}
-          >
-            <h2>RainbowKit Examples &rarr;</h2>
-            <p>Discover boilerplate example RainbowKit projects.</p>
-          </a>
-
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Next.js Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Next.js Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {!(typeof window === "undefined") && web3Provider && (
+          <DynamicBridge
+            // @ts-ignore
+            provider={web3Provider}
+            API_KEY="645b2c8c-5825-4930-baf3-d9b997fcd88c"
+          />
+        )}
       </main>
 
       <footer className={styles.footer}>
